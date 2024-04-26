740f80a6cc8eee00d6fca9cdb0798728119eec44
Bibek Gautam <beebayk0001@gmail.com>
2024-04-27 01:14:30 +0545

    fix: mf

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -7,7 +7,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["quizzer-production-35fb.up.railway.app", "quiz.bibek0001.com.np"]
 
-CSRF_TRUSTED_ORIGINS = ["https://quizzer-production-35fb.up.railway.app", "https:quiz.bibek0001.com.np"]
+CSRF_TRUSTED_ORIGINS = ["https://quizzer-production-35fb.up.railway.app", "https://quiz.bibek0001.com.np"]
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
