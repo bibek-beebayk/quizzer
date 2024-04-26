e74342403e5695c7a39b1d2dd74d09eb20dbf1c4
Bibek Gautam <beebayk0001@gmail.com>
2024-04-26 18:15:19 +0545

    feat: add questions count to category cards

--- apps/qna/views.py
+++ apps/qna/views.py
@@ -7,7 +7,7 @@ from django.db.models import Count
 def index(request):
     context = {}
     random_question = Question.random_question()
-    categories = Category.objects.annotate(questions_count=Count('questions')).filter(questions_count__gt=0)
+    categories = Category.objects.annotate(questions_count=Count('questions')).filter(questions_count__gt=0).order_by("name")
     context["random_question"] = random_question
     context["random_question_correct_answer"] = random_question.answers.get(is_correct=True)
     context["categories"] = categories 
