7ba448b10eff0ddeb1167a00ae7597492c97e109
Bibek Gautam <beebayk0001@gmail.com>
2025-01-17 13:47:09 +0000

    feat: updates

--- apps/qna/views.py
+++ apps/qna/views.py
@@ -9,8 +9,9 @@ def index(request):
     context = {}
     random_question = Question.random_question()
     categories = Category.objects.annotate(questions_count=Count('questions')).filter(questions_count__gt=0).order_by("name")
-    context["random_question"] = random_question
-    context["random_question_correct_answer"] = random_question.answers.get(is_correct=True)
+    if random_question:
+        context["random_question"] = random_question
+        context["random_question_correct_answer"] = random_question.answers.get(is_correct=True)
     context["categories"] = categories 
     return render(request, 'index.html', context)
 
--- core/settings/base.py
+++ core/settings/base.py
@@ -3,6 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
