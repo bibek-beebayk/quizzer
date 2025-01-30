1616cb9c948d71155aecbe2806dfb5c261a04457
Bibek Gautam <beebayk0001@gmail.com>
2025-01-30 05:31:28 +0000

    feat: set pwa orientation

--- core/settings/base.py
+++ core/settings/base.py
@@ -99,6 +99,7 @@ PWA_APP_BACKGROUND_COLOR = "#ffffff"
 PWA_APP_DISPLAY = "standalone"
 PWA_APP_SCOPE = "/"
 PWA_APP_START_URL = "/"
+PWA_APP_ORIENTATION = 'portrait'
 PWA_APP_ICONS = [
     {
         "src": "/static/images/appicon.png",
