2505b6312dca658c9fad124200296374e2917359
Bibek Gautam <beebayk0001@gmail.com>
2025-02-03 04:46:12 +0000

    fix: mf

--- apps/qna/models.py
+++ apps/qna/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
