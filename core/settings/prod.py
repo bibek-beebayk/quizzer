38e026ab8b8e429c434865801d12b33a4f0d4197
Bibek Gautam <beebayk0001@gmail.com>
2025-02-09 13:42:01 +0000

    chore: update cors origins in prod

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -5,18 +5,36 @@ SECRET_KEY = "django-insecure-i()o2=lv+0fj6f*=ha=rcuxc!g74$kj&t-q9dhh7i*sm6!+0!q
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["quizzer-production-35fb.up.railway.app", "quiz.bibek0001.com.np", '127.0.0.1', 'localhost', "quiznfacts.com"]
+ALLOWED_HOSTS = [
+    "quizzer-production-35fb.up.railway.app",
+    "quiz.bibek0001.com.np",
+    "127.0.0.1",
+    "localhost",
+    "quiznfacts.com",
+]
 
-CSRF_TRUSTED_ORIGINS = ["https://quizzer-production-35fb.up.railway.app", "https://quiz.bibek0001.com.np", "https://quiznfacts.com"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://quizzer-production-35fb.up.railway.app",
+    "https://quiz.bibek0001.com.np",
+    "https://quiznfacts.com",
+    "https://localhost:8081",
+    "https://127.0.0.1:8081",
+]
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-AWS_ACCESS_KEY_ID = '1f8bcff6b47d61799db7beaae02a3fa4'
-AWS_SECRET_ACCESS_KEY = '78e32042ccce9896aa5b5767338949146eda472fd532328302be87d2b0ed5260'
-AWS_STORAGE_BUCKET_NAME = 'quizzer'
-AWS_S3_ENDPOINT_URL = 'https://b46a64eb384beb50a5fc80946bc0abc7.r2.cloudflarestorage.com'
+AWS_ACCESS_KEY_ID = "1f8bcff6b47d61799db7beaae02a3fa4"
+AWS_SECRET_ACCESS_KEY = (
+    "78e32042ccce9896aa5b5767338949146eda472fd532328302be87d2b0ed5260"
+)
+AWS_STORAGE_BUCKET_NAME = "quizzer"
+AWS_S3_ENDPOINT_URL = (
+    "https://b46a64eb384beb50a5fc80946bc0abc7.r2.cloudflarestorage.com"
+)
 # AWS_S3_PUBLIC_URL = "https://storage.worldstories.net"
-AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+CORS_ALLOWED_ORIGINS = ["https://localhost:8081", "https://127.0.0.1:8081"]
 
 DATABASES = {
     "default": {
