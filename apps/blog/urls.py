95d2d02bbf7d19432cd310e9b4beb033866730af
Bibek Gautam <beebayk0001@gmail.com>
2025-02-24 02:30:03 +0000

    chore: add ads.txt file

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import blog_list_view, blog_detail_view
+from .views import blog_list_view, blog_detail_view, edit_comment, delete_comment
 
 urlpatterns = [
     path("blogs/", blog_list_view, name="blogs"),
     path("blogs/<slug:slug>/", blog_detail_view, name="blog"),
+    path("edit-comment/", edit_comment, name="edit_comment"),
+    path("delete-comment/", delete_comment, name="delete_comment"),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,17 +1,18 @@
+from http.client import HTTPResponse
+from tokenize import Comment
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
 from apps.analytics.models import PageVisit
-from apps.blog.models import Blog
+from apps.blog.models import Blog, Comment
+from django.views.decorators.http import require_POST
 
 
 def blog_list_view(request):
     context = {}
-    blogs = (
-        Blog.published().prefetch_related("categories")
-        .order_by("-publish_at")
-    )
+    blogs = Blog.published().prefetch_related("categories").order_by("-publish_at")
     paginator = Paginator(blogs, 10)
     page = request.GET.get("page")
     try:
@@ -30,10 +31,29 @@ def blog_detail_view(request, slug):
     context = {}
     blog = Blog.objects.get(slug=slug)
     context["blog"] = blog
-    context["related_posts"] = Blog.published().exclude(id=blog.id).order_by("-publish_at")[:5]
+    context["related_posts"] = (
+        Blog.published().exclude(id=blog.id).order_by("-publish_at")[:5]
+    )
     if request.user.is_authenticated:
         context["has_commented"] = blog.comments.filter(user=request.user).exists()
-    
+
     PageVisit.create_object(request, blog.id)
     return render(request, "blog/detail.html", context)
 
+
+@require_POST
+def edit_comment(request):
+    comment_id = request.POST.get("comment_id")
+    comment_text = request.POST.get("comment")
+    comment = get_object_or_404(Comment, id=comment_id)
+    comment.text = comment_text
+    comment.save()
+    return redirect("blog", slug=Blog.objects.get(id=comment_id).slug)
+
+
+@require_POST
+def delete_comment(request):
+    comment_id = request.POST.get("comment_id")
+    comment = get_object_or_404(Comment, id=comment_id)
+    comment.delete()
+    return redirect("blog", slug=Blog.objects.get(id=comment_id).slug)
